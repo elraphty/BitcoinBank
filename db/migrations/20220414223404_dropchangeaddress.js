7d87310376721931f1d822475cb6f9bfe96930cf
elraphty <elraphtyofficial@gmail.com>
2022-04-14 23:43:57 +0100

    dropped change address column

---
+++ db/migrations/20220414223404_dropchangeaddress.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('useraddresses', (t) => {
+    t.dropColumn('changeaddress');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('useraddresses');
+};
